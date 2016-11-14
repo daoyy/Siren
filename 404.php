74ec2e3d28379a554e3541255d65b38f936b7a1f
Louie <louie.sns@gmail.com>
2016-11-14 15:03:15 +0800

    Compatibility optimization

--- 404.php
+++ 404.php
@@ -7,27 +7,44 @@
  * @package Akina
  */
 
- ?>
+?>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title itemprop="name"><?php global $page, $paged;wp_title( '-', true, 'right' );
-bloginfo( 'name' );$site_description = get_bloginfo( 'description', 'display' );
-if ( $site_description && ( is_home() || is_front_page() ) ) echo " - $site_description";if ( $paged >= 2 || $page >= 2 ) echo ' - ' . sprintf( __( '第 %s 页'), max( $paged, $page ) );?>
-</title>
-<?php wp_head(); ?>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title itemprop="name"><?php global $page, $paged;
+        wp_title('|', true, 'right');
+        bloginfo('name');
+        $site_description = get_bloginfo('description', 'display');
+        if ($site_description && (is_home() || is_front_page())) echo " | $site_description";
+        if ($paged >= 2 || $page >= 2) echo ' | ' . sprintf(__('第 %s 页'), max($paged, $page)); ?>
+    </title>
+    <META content="MSHTML 6.00.2800.1458" name=GENERATOR>
 </head>
-<body <?php body_class(); ?>>
-<section class="error-404 not-found">
-<div class="error-img">
-<img src="<?php echo get_template_directory_uri(); ?>/images/404.jpg">
-</div>
-<div class="err-button back">
-<a id="golast" href=javascript:history.go(-1);>返回上一页</a>
-<a id="gohome" href="<?php bloginfo('url');?>">返回主页</a>  
-</div>
-</section>
+<BODY topMargin=20>
+<TABLE cellSpacing=0 cellPadding=0 width="100%">
+    <TBODY>
+    <TR>
+        <TD
+            style="BACKGROUND: #FB9966; HEIGHT: 1px">&nbsp;</TD>
+    </TR>
+    </TBODY>
+</TABLE>
+<p align="center"><img src="<?php bloginfo('template_url'); ?>/images/404.png" width="791" height="430" border="0" usemap="#Map"
+                       longdesc="<?php bloginfo('url'); ?>"/>
+    <map name="Map" id="Map">
+        <area shape="rect" coords="26,256,428,324" href="<?php bloginfo('url'); ?>"/>
+    </map>
+</p>
+
+<TABLE cellSpacing=0 cellPadding=0 width="100%">
+    <TBODY>
+    <TR>
+        <TD
+            style="BACKGROUND: #FB9966; HEIGHT: 1px">&nbsp;</TD>
+    </TR>
+    </TBODY>
+</TABLE>
 </body>
 
 
